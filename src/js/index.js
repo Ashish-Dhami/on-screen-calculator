ab936a9143339e4d432ad586dac4318c5cd0d97a
Ashish Dhami <ashish.dhami.career@gmail.com>
2026-01-20 04:12:06 +0530

    Add a “backspace” button to calculator
    Added a backspace button, so the user can undo their last input if they
    click the wrong number.

--- src/js/index.js
+++ src/js/index.js
@@ -40,24 +40,57 @@ function renderResult(chained = true) {
     ? result.toString()
     : parseFloat(result.toFixed(6)).toString();
   display.textContent = roundedResult;
-  return chained ? roundedResult : "";
+  if (!chained) {
+    ans_preview.textContent = roundedResult;
+    hidden_preview.classList.remove("hidden");
+    return "";
+  }
+  return roundedResult;
 }
 function init(displayText = "0") {
-  num1 = "";
+  num1 = "0";
   num2 = "";
   operator = null;
   display.textContent = displayText;
+  updatePreview();
 }
 function logger() {
   console.log({ num1, num2, operator });
 }
+function updatePreview() {
+  num1_preview.textContent = num1;
+  num2_preview.textContent = num2;
+  op_preview.textContent = operator;
+  if (num1 || num2 || operator) {
+    ans_preview.textContent = "";
+    hidden_preview.classList.add("hidden");
+  }
+}
 
 const display = document.querySelector(".calculator__display");
 const buttons = document.querySelector(".calculator__functions");
+const num1_preview = document.querySelector(".preview .num1");
+const num2_preview = document.querySelector(".preview .num2");
+const op_preview = document.querySelector(".preview .operator");
+const ans_preview = document.querySelector(".preview .ans");
+const hidden_preview = document.querySelector(".preview .hidden");
 buttons.addEventListener("click", (e) => {
   let val = e.target.value;
   if (val == "clear") {
     init();
+    updatePreview();
+    return;
+  }
+  if (val == "backspace") {
+    if (!operator) {
+      num1 = num1.slice(0, -1).padEnd(1, "0");
+      display.textContent = num1;
+    } else {
+      num2 = num2.slice(0, -1);
+      display.textContent = num2 || num1;
+    }
+    updatePreview();
+    logger();
     return;
   }
   if (isNaN(val)) {
@@ -69,12 +102,13 @@ buttons.addEventListener("click", (e) => {
   } else {
     if (!operator) {
       num1 += val;
-      display.textContent = parseFloat(num1).toString();
+      display.textContent = num1 = parseFloat(num1).toString();
     } else {
       num2 += val;
-      display.textContent = parseFloat(num2).toString();
+      display.textContent = num2 = parseFloat(num2).toString();
     }
   }
+  updatePreview();
   logger();
 });
 
@@ -82,3 +116,5 @@ let num1, num2, operator;
 init();
 
 //todo: -ve number support
+//todo: float number support
+//todo: keyboard support
